2b67c94425c8d9a6e7d7edc9a5c74cf006b8485a
wp-palace <edeveloper3@gmail.com>
2017-06-16 20:52:41 -0400

    game is completed

--- src/index.js
+++ src/index.js
@@ -10,12 +10,36 @@ function Square( props ) {
 	);
 }
 
+function calculateWinner( squares ) {
+	const lines = [
+		[ 0, 1, 2 ],
+		[ 3, 4, 5 ],
+		[ 6, 7, 8 ],
+		[ 0, 3, 6 ],
+		[ 1, 4, 7 ],
+		[ 2, 5, 8 ],
+		[ 0, 4, 8 ],
+		[ 2, 4, 6 ]
+	];
+
+	for ( let line of lines ) {
+		const [ a, b, c ] = line;
+
+		if ( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+			return squares[a];
+		}
+	}
+
+	return null;
+}
+
 class Board extends React.Component {
 	constructor() {
 		super();
 
 		this.state = {
-			squares: Array( 9 ).fill( null )
+			squares: Array( 9 ).fill( null ),
+			xIsNext: true
 		};
 	}
 
@@ -25,13 +49,27 @@ class Board extends React.Component {
 
 	handleClick( i ) {
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
+		
+		if ( calculateWinner( squares ) || squares[ i ] )
+			return;
 
-		this.setState( { squares: squares } )
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+		this.setState( { 
+			squares: squares, 
+			xIsNext: !this.state.xIsNext 
+		} );
 	}
 
 	render() {
-		const status = 'Next player: X';
+		const winner = calculateWinner( this.state.squares );
+
+		let status;
+
+		if ( winner )
+			status = 'Winner: ' + winner;
+		else
+			status = 'Next player: ' + ( this.state.xIsNext ? 'X' : 'O' );
 
 		return (
 			<div>

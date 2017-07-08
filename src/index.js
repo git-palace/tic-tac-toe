5787f54fbded20564baf2d266fed88e8ef7b0805
wp-palace <edeveloper3@gmail.com>
2017-07-08 09:33:15 -0400

    'final'

--- src/index.js
+++ src/index.js
@@ -70,11 +70,19 @@ class Game extends React.Component {
 				squares: Array(9).fill( null )
 			}],
 			xIsNext: true,
+			stepNumber: 0
 		}
 	}
 
+	jumpTo( step ) {
+		this.setState( {
+			stepNumber: step,
+			xIsNext: (step % 2) === 0
+		} );
+	}
+
 	handleClick( i ) {
-		const history = this.state.history;
+		const history = this.state.history.slice( 0, this.state.stepNumber + 1 );
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		
@@ -87,14 +95,25 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares,
 			}]),
-			xIsNext: !this.state.xIsNext 
+			xIsNext: !this.state.xIsNext,
+			stepNumber: history.length
 		} );
 	}
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner( current.squares );
+
+		const moves = history.map( ( step, move ) => {
+			const desc = move ? 'Move #' + move : 'Game Start';
+
+			return (
+				<li key={move}>
+					<a href="#" onClick={ () => this.jumpTo( move ) }>{desc}</a>
+				</li>
+			)
+		} );
 
 		let status;
 
@@ -113,7 +132,7 @@ class Game extends React.Component {
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
-					<ol>{/* TODO */}</ol>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
